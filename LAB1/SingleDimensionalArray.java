04a84298b2717f1ad4c7e6af21e2275dec089713
Sushant Rahapal <53401773+rahapal@users.noreply.github.com>
2020-02-13 22:10:17 +0545

    Create SingleDimensionalArray.java

---
+++ LAB1/SingleDimensionalArray.java
@@ -0,0 +1,14 @@
+```javascript
+class Testarray{  
+public static void main(String args[]){  
+int a[]={33,3,4,5};
+for(int i=0;i<a.length;i++)//length is the property of array  
+System.out.println(a[i]);  
+}} 
+``` 
+
+### Output
+33\
+3\
+4\
+5
