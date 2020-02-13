beb084fb5942750a329f7a4daee99c4bbd8fef70
Sushant Rahapal <53401773+rahapal@users.noreply.github.com>
2020-02-13 22:11:17 +0545

    Update SingleDimensionalArray.java

--- LAB1/SingleDimensionalArray.java
+++ LAB1/SingleDimensionalArray.java
@@ -2,7 +2,7 @@
 class Testarray{  
 public static void main(String args[]){  
 int a[]={33,3,4,5};
-for(int i=0;i<a.length;i++)//length is the property of array  
+for(int i=0;i<a.length;i++)
 System.out.println(a[i]);  
 }} 
 
