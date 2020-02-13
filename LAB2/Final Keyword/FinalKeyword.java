d09b33b79de464206458c2211b7fa8e56a8baf7b
Sushant Rahapal <53401773+rahapal@users.noreply.github.com>
2020-02-13 22:44:01 +0545

    Lab work

---
+++ LAB2/Final Keyword/FinalKeyword.java
@@ -0,0 +1,20 @@
+class FinalVariable
+
+{
+
+   public static void main(String[] args)
+
+   {
+
+      final int hours=24;
+
+      System.out.println("Hours in 6 days = " + hours * 6);
+
+   }
+
+}
+
+
+Output:
+
+Hours in 6 days = 144
